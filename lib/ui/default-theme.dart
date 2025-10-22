c6a66a64cce3403680362c1aa79950ecf3cb645a
vavnastya <vav.nastya2007@gmail.com>
2025-10-22 12:28:09 +0300

    Создание классов

---
+++ lib/ui/default-theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomThemes {
+  static ThemeData get defaultTheme {
+    return ThemeData(
+      primaryColor: Color(0x2761F5),
+      scaffoldBackgroundColor: Color(0x26282B),
+    );
+  }
+}
